e56eb3979cd256713f5fe2db97aa816ca2ab7665
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-05-09 13:44:52 +0530

    Updating User.tsx

---
+++ src/components/user/User.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./user.css";
+
+function User() {
+    return (
+        <div>
+            <h2>User List</h2>
+
+        </div>
+    );
+}
+
+export default User
