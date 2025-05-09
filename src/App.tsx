74adbb1040052107567edc4cafe50dda60e7ff58
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-05-09 14:29:34 +0530

    updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/counter/Counter';
+import User from './components/user/User';
 
 function App() {
   return (
     <>
       <Counter />
+      <br /><br /><br /><br />
+      <User />
     </>
   );
 }
