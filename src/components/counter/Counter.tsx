603fd61f0ed3f5630a072bb364a80b1f0df24ec2
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-05-09 12:47:35 +0530

    Created  counter component

---
+++ src/components/counter/Counter.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import "./counter.css"
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    function handleIncrement(): void {
+        setCount(count + 1);
+    }
+
+    function handleDecrement(): void {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+
+    function handleReset(): void {
+        setCount(0);
+    }
+
+    return (
+        <div className='counter'>
+            <h1 className='counter-title'>Counter</h1>
+            <p className='counter-value'>Counter: {count} </p>
+            <div className='counter-buttons'>
+                <button onClick={handleIncrement}>➕</button>
+                <button onClick={handleDecrement}>➖</button>
+                <button onClick={handleReset}>🔁</button>
+            </div>
+        </div>
+    );
+}
+
+export default Counter
