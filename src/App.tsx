d43535daf401ba9d06422274669cf50678a6eddc
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-05-10 13:59:28 +0530

    Added ThemeToggle to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import Home from './components/Home';
 import Counter from './components/counter/Counter';
 import User from './components/user/User';
 import UserForm from './components/form/UserForm';
+import ThemeToggle from './components/theme/ThemeToggle';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/form" element={<UserForm />} />
+        <ThemeToggle />
+        <div>
+          <Route path="/" element={<Home />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/form" element={<UserForm />} />
+        </div>
       </Routes>
     </>
   );
