1309efc2ab96e63ff411d5d61333dadafc921dc8
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-05-14 16:47:16 +0530

    added ThemeToggle Component in to 'src/components/Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import ThemeToggle from './theme/ThemeToggle';
 
 const Home: React.FC = () => {
-
     return (
-        <div>
-            <Link to={"./counter/Counter.tsx"}>Counter</Link>
-            <br /><br /><br /><br />
-            <Link to={"./user/user.tsx"}>Users</Link>
+        <div className='home-container'>
+            <ThemeToggle />
+            <nav className='nav-links'>
+                <Link to={"./counter/Counter.tsx"}>Counter</Link>
+                <Link to={"./user/user.tsx"}>Users</Link>
+                <Link to={"./form/UserForm.tsx"}>Add User</Link>
+            </nav>
         </div>
     );
 }
